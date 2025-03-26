208bf7b8578753e1c544f4b8b772de7aa55cdb8e
ngochieu276 <ngochieu2761998@gmail.com>
2025-03-26 16:36:32 +0700

    finish UI

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,3 +1,167 @@
+import { Card } from "@/components/card";
+import SectionHeader from "@/components/section-header";
+import bookingImage from "@/assets/images/book-cover.png";
+import Image from "next/image";
+
+import JavascriptIcon from "@/assets/icons/square-js.svg";
+import HTMLIcon from "@/assets/icons/html5.svg";
+import CssIcon from "@/assets/icons/css3.svg";
+import ReactIcon from "@/assets/icons/react.svg";
+import ChromeIcon from "@/assets/icons/chrome.svg";
+import GithubIcon from "@/assets/icons/github.svg";
+import TechIcon from "@/components/tech-icon";
+import mapImage from "@/assets/images/map.png";
+import smileEmoji from "@/assets/images/memoji-smile.png";
+import { CardHeader } from "@/components/card-header";
+import ToolboxItems from "@/components/toolbox-items";
+
+const toolboxItems = [
+  {
+    title: "JavaScript",
+    iconType: JavascriptIcon,
+  },
+  {
+    title: "HTML5",
+    iconType: HTMLIcon,
+  },
+  {
+    title: "CSS3",
+    iconType: CssIcon,
+  },
+  {
+    title: "React",
+    iconType: ReactIcon,
+  },
+  {
+    title: "Chrome",
+    iconType: ChromeIcon,
+  },
+  {
+    title: "Github",
+    iconType: GithubIcon,
+  },
+];
+
+const hobbies = [
+  {
+    title: "Painting",
+    emoji: "🎨",
+    left: "5%",
+    top: "5%",
+  },
+  {
+    title: "Photography",
+    emoji: "📷",
+    left: "50%",
+    top: "5%",
+  },
+  {
+    title: "Gaming",
+    emoji: "🎮",
+    left: "10%",
+    top: "35%",
+  },
+  {
+    title: "Hiking",
+    emoji: "🥾",
+    left: "35%",
+    top: "45%",
+  },
+  {
+    title: "Music",
+    emoji: "🎵",
+    left: "70%",
+    top: "45%",
+  },
+  {
+    title: "Fitness",
+    emoji: "🏋️",
+    left: "5%",
+    top: "65%",
+  },
+  {
+    title: "Reading",
+    emoji: "📖",
+    left: "45%",
+    top: "70%",
+  },
+];
+
 export const AboutSection = () => {
-  return <div>About Section</div>;
+  return (
+    <div className="py-20 bg:py-28">
+      <div className="container">
+        <SectionHeader
+          eyebrow="About Me"
+          title="A Glimpse Into my world"
+          description="Learn more about who I am, what I do"
+        />
+        <div className="mt-20 flex flex-col gap-8">
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-5 ">
+            <Card className="h-[320px] md:col-span-2">
+              <CardHeader
+                title="My Reads"
+                description="Explore the books shaping my perspective."
+              />
+              <div className="w-40 mx-auto mt-2 md:mt-0">
+                <Image src={bookingImage} alt="Book cover" />
+              </div>
+            </Card>
+            <Card className="h-[320px] md:col-span-3">
+              <CardHeader
+                title="My Toolbox"
+                description="Explore the technologies and tools I use to create digital
+                experiences."
+              />
+              <ToolboxItems items={toolboxItems} className="mt-6" />
+              <ToolboxItems
+                items={toolboxItems}
+                className="mt-6"
+                itemWrapperClassName="-translate-x-1/2"
+              />
+            </Card>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-3 gap-8">
+            <Card className="h-[320px] p-0 flex flex-col md:col-span-3 lg:col-span-2">
+              <CardHeader
+                title="Beyond the Code"
+                description="Explore my interests and hobbies beyond the digital."
+                className="px-6 py-6"
+              />
+              <div className="relative flex-1">
+                {hobbies.map((hobby) => (
+                  <div
+                    key={hobby.title}
+                    className="absolute inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5"
+                    style={{
+                      left: hobby.left,
+                      top: hobby.top,
+                    }}
+                  >
+                    <span className="font-medium text-gray-950">
+                      {hobby.title}
+                    </span>
+                    <span>{hobby.emoji}</span>
+                  </div>
+                ))}
+              </div>
+            </Card>
+            <Card className="h-[320px] p-0  md:col-span-2 lg:col-span-1">
+              <Image
+                src={mapImage}
+                alt="map"
+                className="h-full w-full object-cover object-left-top"
+              />
+              <div
+                className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 
+            after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:outline-gray-950/30 after:rounded-full"
+              >
+                <Image src={smileEmoji} alt="smile" />
+              </div>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,3 +1,36 @@
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+import grainImage from "@/assets/images/grain.jpg";
+
 export const ContactSection = () => {
-  return <div>Contact Section</div>;
+  return (
+    <div className="py-16 pt-12">
+      <div className="container ">
+        <div className="relative bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 text-center md:text-left py-8 px-8 rounded-3xl overflow-hidden z-0">
+          <div
+            className="absolute inset-0 opacity-15 -z-10"
+            style={{
+              backgroundImage: `url(${grainImage.src})`,
+            }}
+          ></div>
+          <div className="flex flex-col gap-8 md:gap-16 items-center md:flex-row">
+            <div>
+              <h2 className="font-serif text-2xl md:text-3xl font-semibold">
+                Let&apos;s create something amazing together
+              </h2>
+              <p className="text-sm mt-2 md:text-base">
+                Ready to bring your next project to life? Let&apos;s connect and
+                discuss how I can help you achieve your goals.
+              </p>
+            </div>
+            <div>
+              <button className="text-white bg-gray-900 inline-flex justify-center items-center px-6 h-12 rounded-xl gap-2 w-max border border-gray-900">
+                <span className="font-semibold">Contact Me</span>
+                <ArrowUpRightIcon className="size-4" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
